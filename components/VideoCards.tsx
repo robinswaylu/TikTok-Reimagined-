97754c2a812380a2a8a52d9f8fc309648130dc4e
Robin Lu <robinswaylu@gmail.com>
2022-07-28 19:05:02 +0800

    implement api to backend + main video componenets

---
+++ components/VideoCards.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VideoCards = () => {
+  return (
+    <div>VideoCards</div>
+  )
+}
+
+export default VideoCards
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,35 @@
 import type { NextPage } from 'next' 
-const Home: NextPage = () => {
+import axios from 'axios'
+import { Video } from '../types'
+import VideoCards from '../components/VideoCards' 
+import NoResults from '../components/NoResults'
+
+interface Iprops {
+  videos: Video[]
+}
+
+const Home = ( { videos } : Iprops ) => {
+  console.log('this is videos: ' , videos) 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='flex flex-col gap-10 videos h-full'>
+    {videos.length 
+      ? videos.map((video: Video) => (
+        <VideoCards post={video} isShowingOnHome key={video._id} />
+      )) 
+      : <NoResults text={`No Videos`} />}
+  </div>
   )
 }
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`); 
+  
+  console.log('this is data: ' + data) 
+  return {
+    props:{ videos: data}
+  }
+}
+
+
 
 export default Home
