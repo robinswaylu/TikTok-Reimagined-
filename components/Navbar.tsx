923881acf6ce5d0c4c9e11870b18faa52eee2108
Robin Lu <robinswaylu@gmail.com>
2022-07-28 17:54:20 +0800

    add nav, footer , and sidebar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'> 
             <Link href="/"> 
-                <div className='w-[100px] md:w-[129px]  '>
+                <div className='w-[100px] md:w-[129px]'>
                     <Image className='cursor-pointer' src={Logo} alt='Tiktik' layout='responsive'/>
                 </div>
             </Link>
