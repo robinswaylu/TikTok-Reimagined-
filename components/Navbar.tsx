7a86bb76a52633e25a3c113bc92e11e88c96ef2e
Robin Lu <robinswaylu@gmail.com>
2022-08-02 19:45:17 +0800

    set up Google react Oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,21 +2,29 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { GoogleLogin, GoogleLogout } from 'react-google-login'
 import { AiOutlineLogout } from 'react-icons/ai'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
 import Logo from '../utils/tiktik-logo.png'
 
 const Navbar = () => {
+
+    const user = false;
+
     return (
-        <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'> 
-            <Link href="/"> 
+        <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
+            <Link href="/">
                 <div className='w-[100px] md:w-[129px]'>
-                    <Image className='cursor-pointer' src={Logo} alt='Tiktik' layout='responsive'/>
+                    <Image className='cursor-pointer' src={Logo} alt='Tiktik' layout='responsive' />
                 </div>
             </Link>
+            <div>SEARCH</div>
+            <div>
+                {user ? ( <div>Logged in</div> ) : ( <GoogleLogin onSuccess={(response)=>  console.log(response)} onError={()=> console.log('error')}/> ) }
+
+            </div>
         </div>
     )
 }
