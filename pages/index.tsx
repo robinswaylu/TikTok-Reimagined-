dc84d13c2df4e11070ea195574818970d5ab8ed9
Robin Lu <robinswaylu@gmail.com>
2022-08-11 18:14:51 +0800

    create search component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next' 
+import type { NextPage } from 'next'
 import axios from 'axios'
 import { Video } from '../types'
-import VideoCards from '../components/VideoCards' 
+import VideoCards from '../components/VideoCards'
 import NoResults from '../components/NoResults'
 import { BASE_URL } from '../utils'
 
@@ -9,25 +9,24 @@ interface Iprops {
   videos: Video[]
 }
 
-const Home = ( { videos } : Iprops ) => {
-  console.log('this is videos: ' , videos) 
+const Home = ({ videos }: Iprops) => {
+  console.log('this is videos: ', videos)
   return (
     <div className='flex flex-col gap-10 videos h-full'>
-    {videos.length 
-      ? videos.map((video: Video) => (
-        <VideoCards post={video}   key={video._id} />
-      )) 
-      : <NoResults text={`No Videos`} />}
-  </div>
+      {videos.length
+        ? videos.map((video: Video) => (
+          <VideoCards post={video} key={video._id} />
+        ))
+        : <NoResults text={`No Videos`} />}
+    </div>
   )
 }
 
 export const getServerSideProps = async () => {
-  const { data } = await axios.get(`${BASE_URL}/api/post`); 
-  
-  console.log('this is data: ' + data) 
+  const { data } = await axios.get(`${BASE_URL}/api/post`);
+
   return {
-    props:{ videos: data}
+    props: { videos: data }
   }
 }
 
