a5d6b5bd8655e2917eb7cdba471c735ecb7d7bc0
Robin Lu <robinswaylu@gmail.com>
2022-08-16 19:30:46 +0800

    add playsInline to video element

--- components/VideoCards.tsx
+++ components/VideoCards.tsx
@@ -99,6 +99,7 @@ const VideoCards: NextPage<IProps> = ({ post }) => {
                             preload="metadata"
                             loop
                             muted
+                            playsInline
                             ref={videoRef}
                             src={post.video.asset.url + '#t=0.1'}
                             className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] rounded-2xl cursor-pointer bg-gray-100'
