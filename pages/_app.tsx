cb359c77047c7e57d61ea6e0013eff683cebd76a
Robin Lu <robinswaylu@gmail.com>
2022-08-16 17:08:27 +0800

    update mobile video card styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
     <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <div className='xl:w-[1200px] m-auto overflow-hidden h-[100vh]'>
         <Navbar />
-        <div className='flex   md:gap-20 '>
+        <div className='flex  gap-2 md:gap-20 '>
           <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
             <Sidebar />
           </div>
